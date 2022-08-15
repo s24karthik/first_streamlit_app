e823a9796e88df6af89b5c8150ddee888350b01f
s24karthik <87420414+s24karthik@users.noreply.github.com>
2022-08-15 14:32:42 +0530

    Update streamlit_app.py
    bug fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ streamlit.dataframe(fruits_to_show)
 
 #create the repeat code block
 def get_fruityvice_data(this_fruit_choice):
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 
@@ -38,7 +38,7 @@ try:
     streamlit.error("Please select a fruit to get information")
   else:
     back_from_function = get_fruityvice_data(fruit_choice)
-    streamlit.dataframe(fruityvice_normalized)
+    streamlit.dataframe(back_from_function)
     
 except URLError as e:
   streamlit.error()
@@ -51,11 +51,11 @@ def get_fruit_load_list():
     return my_cur.fetchall()
 
 #Add a button to load fruit
-if streamlit.button('Get Fruit List'):
+if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
   my_cnx.close()
-  streamlit.dataframe(fruityvice_normalized)
+  streamlit.dataframe(my_data_rows)
 
 #allow the user to add to the list
 def insert_row_snowflake(new_fruit):
@@ -64,7 +64,7 @@ def insert_row_snowflake(new_fruit):
     return "Thanks for adding " + new_fruit  
   
 add_my_fruit = streamlit.text_input("What fruit would you like to add")
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Get Fruit to the List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
   streamlit.text(back_from_function)
