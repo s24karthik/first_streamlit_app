10c92e6b02e5f89851c81998bc71842732a43b9e
s24karthik <87420414+s24karthik@users.noreply.github.com>
2022-08-15 14:17:23 +0530

    Update streamlit_app.py
    bug fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,7 +67,6 @@ add_my_fruit = streamlit.text_input("What fruit would you like to add")
 if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
-  my_cnx.close()
   streamlit.text(back_from_function)
  
   
